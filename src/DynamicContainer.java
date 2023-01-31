c3ccef45ed23bf10edd143be3ace554992d1a648
johnDoCodigo <122378532+johnDoCodigo@users.noreply.github.com>
2023-01-31 11:31:15 +0000

    2nd commit

--- src/DynamicContainer.java
+++ src/DynamicContainer.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class DynamicContainer {
+public class DynamicContainer {
+    int[] numbers ;
+
+    public DynamicContainer() {
+        this.numbers = new int [5];
+    }
+    public class Node {
+        private void add(int data) {
+
+        }
+
+        private void remove(int data) {
+
+        }
+
+        private void printList() {
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DynamicContainer dynamicContainer = new DynamicContainer();
+        System.out.println(dynamicContainer.numbers.length);
+
+
+        DynamicContainer.
+
+
+
+
     }
 }
